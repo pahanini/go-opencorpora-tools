9bd4c8646ff89df7ccac001123be13cafabbafb2
Shirshova Anastasiya Vyacheslavovna <a.v.shirshova@tinkoff.ru>
2021-03-11 10:50:51 +0300

    dictionary parsing fixes

--- morph_test.go
+++ morph_test.go
@@ -2,8 +2,9 @@ package opencorpora
 
 import (
 	"bytes"
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 const dict = `
@@ -20,19 +21,46 @@ const dict = `
 							<alias>СУЩ</alias>
 							<description>имя существительное</description>
 					</grammeme>
+					<grammeme parent="ANim">
+						<name>anim</name>
+						<alias>од</alias>
+						<description>одушевлённое</description>
+					</grammeme>
+					<grammeme parent="NMbr">
+						<name>sing</name>
+						<alias>ед</alias>
+						<description>единственное число</description>
+					</grammeme>
+					<grammeme parent="CAse">
+						<name>nomn</name>
+						<alias>им</alias>
+						<description>именительный падеж</description>
+					</grammeme>
+					<grammeme parent="CAse">
+						<name>gent</name>
+						<alias>рд</alias>
+						<description>родительный падеж</description>
+					</grammeme>
+					<grammeme parent="ms-f">
+						<name>masc</name>
+						<alias>мр</alias>
+						<description>мужской род</description>
+					</grammeme>
 			</grammemes>
 			<lemmata>
 					<lemma id="1" rev="1">
 							<l t="ёж">
-									<g v="POST"/>
-									<g v="NOUN"/>
+								<g v="NOUN"/>
+								<g v="anim"/>
+								<g v="masc"/>
 							</l>
 							<f t="ёж">
-									<g v="NOUN"/>
+								<g v="sing"/>
+								<g v="nomn"/>
 							</f>
 							<f t="ежа">
-									<g v="POST"/>
-									<g v="NOUN"/>
+								<g v="sing"/>
+								<g v="gent"/>
 							</f>
 					</lemma>
 			</lemmata>
@@ -51,6 +79,6 @@ func TestMorth(t *testing.T) {
 
 	tag, err := m.Tag("ежа")
 	require.NoError(t, err)
-	require.Len(t, tag, 2)
-	require.Equal(t, "POST", tag[0].Name)
+	require.Len(t, tag[0], 5)
+	require.Equal(t, "NOUN", tag[0][0].Name)
 }
